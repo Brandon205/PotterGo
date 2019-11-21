7f96ffb7890d29ea2f1118c0de715ef7747415ec
Brandon205 <brandonblack02@gmail.com>
2019-11-21 11:57:49 -0800

    Homepage made

---
+++ client/src/Homepage.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Map from './Mapbox';
+
+export default function Homepage(props) {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  )
+}
