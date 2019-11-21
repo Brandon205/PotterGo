73e204a98e7481ea786c41b4bd8dfc4f24b8bc16
cerebrium <nicholas.m.shankland@gmail.com>
2019-11-21 12:04:15 -0800

    changed name of one comonent

---
+++ client/src/DiagonAlley.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function DiagonAlley () {
+
+    const [houses, setHouses] = useState([])
+
+    useEffect(() => {
+        axios.get('/api').then(response => {
+            console.log(response.data)
+            setHouses(response.data)
+        }) 
+    }, [])
+
+    const hogwartsHouses = houses.map((ele, id) => <p key={id}>{ele.name}</p>)
+
+    return (
+        <>
+            <h1>Api Page</h1>
+            {hogwartsHouses}
+        </>
+    )
+}
+
+export default DiagonAlley
