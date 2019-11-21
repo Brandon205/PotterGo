2472d134a3834396c7b4cbc95a00a52654c36910
Brandon205 <brandonblack02@gmail.com>
2019-11-21 12:05:42 -0800

    No more mapbox

--- client/src/Homepage.jsx
+++ client/src/Homepage.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import Map from './Mapbox';
-
-export default function Homepage(props) {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  )
-}
-=======
 import React from 'react'
 
 function Homepage () {
@@ -20,4 +8,3 @@ function Homepage () {
 
 
 export default Homepage
->>>>>>> 4009d77d19e78172d45abd813a84e3b7ee6709ce
